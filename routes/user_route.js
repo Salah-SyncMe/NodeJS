f234569dfac21fe45eff808c3c0fc2984ef14915
SalahmustafaY <132152831+SalahProgramer@users.noreply.github.com>
2024-09-03 05:55:19 +0300

    add image

--- routes/user_route.js
+++ routes/user_route.js
@@ -7,7 +7,7 @@ userRoute.get("/",userController.fetchUsers);
 userRoute.get("/:id",userController.fetchById); //ex:http://localhost:5000/user/66cdf8873c9f3d6af22d58be
 
 //insert an user into database route
-userRoute.post("/signup",upload.single("image"),userController.userSignUp);
+userRoute.post("/signup",userController.userSignUp);
 userRoute.post("/login",userController.userLogin);
 
 
