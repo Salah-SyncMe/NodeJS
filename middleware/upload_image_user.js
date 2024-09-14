5364324e600ed0917938d7a414c7de9f2d8b6e71
SalahmustafaY <132152831+SalahProgramer@users.noreply.github.com>
2024-09-14 18:19:52 +0300

    add4

--- middleware/upload_image_user.js
+++ middleware/upload_image_user.js
@@ -2,7 +2,6 @@ const path=require("path");
 const multer=require("multer");
 
 var storage=multer.diskStorage({
-
 filename: function(request,file,cb){
 let ext=path.extname(file.originalname);
 const email= request.body.email;
--- middleware/upload_images_post.js
+++ middleware/upload_images_post.js
@@ -2,13 +2,6 @@ const path=require("path");
 const multer=require("multer");
 
 var storagePost=multer.diskStorage({
-destination: function(request,file,cb){//callback
-
-cb(null,path.join(__dirname,'../uploads/imagesPosts'));
-
-},
-
-
 
 filename: function(request,file,cb){
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
